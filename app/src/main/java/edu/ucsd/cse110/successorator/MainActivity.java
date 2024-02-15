c0d738997335bc67886a006de986b26f146b6f7e
Sungyoung <sungyoung@sungyoungsmbp8.lan>
2024-02-15 00:41:20 -0800

    Add current date and day of the week

--- app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
@@ -1,12 +1,17 @@
 package edu.ucsd.cse110.successorator;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.ucsd.cse110.successorator.databinding.ActivityMainBinding;
 import edu.ucsd.cse110.successorator.ui.goals.GoalsFragment;
 import edu.ucsd.cse110.successorator.ui.goals.dialog.CreateGoalDialogFragment;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -27,5 +32,11 @@ public class MainActivity extends AppCompatActivity
             CreateGoalDialogFragment dialogFragment = CreateGoalDialogFragment.newInstance();
             dialogFragment.show(getSupportFragmentManager(), "CreateGoalDialogFragment");
         });
+
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+        TextView textViewDate = findViewById(R.id.text_view_date);
+        textViewDate.setText(currentDate);
+
     }
 }
