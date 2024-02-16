17d5047e7b12bc8042621790baa6b5caae6ac359
warrenli0 <kleinalo10@gmail.com>
2024-02-16 14:27:57 -0800

    Remove logging statements

--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/data/InMemoryDataSource.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/data/InMemoryDataSource.java
@@ -171,17 +171,6 @@ public class InMemoryDataSource {
                 .map(Goal::sortOrder)
                 .collect(Collectors.toList());
 
-        for (Integer sortOrder : sortOrders) {
-            if (sortOrder < 0) {
-                // Log the negative value or throw an exception
-                // Use your preferred logging method. For simplicity, we're using System.out here
-                System.out.println("Negative sort order found: " + sortOrder);
-                // Alternatively, you could throw an exception to fail the test and show the value
-                throw new AssertionError("Negative sort order found: " + sortOrder);
-            }
-        }
-
-
         // Non-negative...
         assert sortOrders.stream().allMatch(i -> i >= 0);
 
